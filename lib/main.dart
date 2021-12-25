5cde4d2b9e5b37a28b30495b4aef08af2e186427
youyoung00 <you889@hanmail.net>
2021-12-26 02:14:34 +0900

    영화 검색앱 안드로이드 버전 http handshake.cc:359 오류 해결 코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:movie_search/ui/movie_screen.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MovieScreen(),
-    );
+    return const MaterialApp(home: MovieScreen());
   }
 }
